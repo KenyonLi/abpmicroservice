90c03aca0007c9e7de7a7dfc3feacaf80b408b6b
Kenyon Li <kenyonli@outlook.com>
2023-09-16 01:25:29 +0800

    -

---
+++ LKN.Microservices/microservices/LKN.User.HttpApi.Host/EntityFrameworkCore/UserHttpApiHostMigrationsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace LKN.User.EntityFrameworkCore;
+
+public class UserHttpApiHostMigrationsDbContext : AbpDbContext<UserHttpApiHostMigrationsDbContext>
+{
+    public UserHttpApiHostMigrationsDbContext(DbContextOptions<UserHttpApiHostMigrationsDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ConfigureUser();
+    }
+}
---
+++ LKN.Microservices/microservices/LKN.User.HttpApi.Host/EntityFrameworkCore/UserHttpApiHostMigrationsDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LKN.User.EntityFrameworkCore;
+
+public class UserHttpApiHostMigrationsDbContextFactory : IDesignTimeDbContextFactory<UserHttpApiHostMigrationsDbContext>
+{
+    public UserHttpApiHostMigrationsDbContext CreateDbContext(string[] args)
+    {
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<UserHttpApiHostMigrationsDbContext>()
+            .UseSqlServer(configuration.GetConnectionString("User"));
+
+        return new UserHttpApiHostMigrationsDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
