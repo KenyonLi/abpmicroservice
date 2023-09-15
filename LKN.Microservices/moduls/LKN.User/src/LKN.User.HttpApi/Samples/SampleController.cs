08d0a9e7de3fc3b29add845a5089631498692338
Kenyon Li <kenyonli@outlook.com>
2023-09-16 01:27:05 +0800

    -

---
+++ LKN.Microservices/moduls/LKN.User/src/LKN.User.HttpApi/Samples/SampleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+
+namespace LKN.User.Samples;
+
+[Area(UserRemoteServiceConsts.ModuleName)]
+[RemoteService(Name = UserRemoteServiceConsts.RemoteServiceName)]
+[Route("api/User/sample")]
+public class SampleController : UserController, ISampleAppService
+{
+    private readonly ISampleAppService _sampleAppService;
+
+    public SampleController(ISampleAppService sampleAppService)
+    {
+        _sampleAppService = sampleAppService;
+    }
+
+    [HttpGet]
+    public async Task<SampleDto> GetAsync()
+    {
+        return await _sampleAppService.GetAsync();
+    }
+
+    [HttpGet]
+    [Route("authorized")]
+    [Authorize]
+    public async Task<SampleDto> GetAuthorizedAsync()
+    {
+        return await _sampleAppService.GetAsync();
+    }
+}
